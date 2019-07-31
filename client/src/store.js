7fff7d466d005493626be24aafe6b705a69e147e
Sarah-Opalenik <sarahmo93@gmail.com>
2019-07-31 10:55:33 -0600

    search function backend

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import { STATES } from 'mongoose';
 
 Vue.use(Vuex)
+//https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+
+let api = axios.create({
+  baseURL: 'https://www.thecocktaildb.com/api/json/v1/1'
+})
+
+let searchurl = 'search.php?s='
 
 export default new Vuex.Store({
   state: {
-
+    queryDrinks: {}
+    // savedDrinks: {}
   },
   mutations: {
+    setQueryDrinks(state, data) {
+      state.queryDrinks = data
+
+    }
 
   },
   actions: {
-
+    async search({ dispatch, commit }, query) {
+      try {
+        let res = await api.get(searchurl + query)
+        console.log(res.data)
+        commit("setQueryDrinks", res.data)
+      } catch (err) { console.error(err) }
+    }
   }
 })
